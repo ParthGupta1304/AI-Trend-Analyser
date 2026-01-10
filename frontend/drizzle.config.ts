609aff3348f3227999a524c7fa5f34ad4d497d59
Parth Gupta <guptaparth1304@gmail.com>
2026-01-10 15:40:50 +0530

    v1.1 fix dotenv

--- frontend/drizzle.config.ts
+++ frontend/drizzle.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from "drizzle-kit";
-import * as dotenv from "dotenv";
-import * as path from "path";
-
-// Load .env.local file
-dotenv.config({ path: path.resolve(__dirname, ".env.local") });
 
 export default defineConfig({
   schema: "./db/schema.ts",
-  out: "./db",
+  out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url:
